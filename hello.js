9debf9965c1fa150c2ea1b941b021c3e923a324d
SJ0900777 <sj0900777@gmail.com>
2026-07-24 03:06:38 +0530

    part 1

---
+++ hello.js
@@ -0,0 +1,27 @@
+console.log("Hello JS");
+console.log("I'm learning JS");
+
+// This is a single line comment
+// javascript is a programming language that is commonly used in web development. It allows developers to create interactive and dynamic content on websites.
+
+// variables : they are named memory locations that store data values. In JavaScript, you can declare variables using var, let, or const keywords.
+
+name = "Tony Stark";
+console.log(name);
+age = 21;
+console.log(age);
+price = 100.5;
+console.log(price);
+x = true;
+console.log(x);
+a = null; //khali value ko null kehte hai
+console.log(a);
+b = undefined; //pata hi nhi ki kya h isme
+console.log(b);
+
+//JS is a dynamically typed language, which means you don't have to specify the data type of a variable when you declare it. The data type is determined automatically based on the value assigned to the variable.
+// variable names in JS are case-sensitive, which means that name and Name would be considered two different variables. Variable names can contain letters, numbers, underscores, and dollar signs, but they cannot start with a number. It's also a good practice to use descriptive variable names that indicate the purpose of the variable.
+//letter , _ or $ se start ho sakta hai variable name, number se start nhi ho sakta
+//let , const , var : ye 3 keywords hai variable declare karne ke liye
+// let : ye variable ko declare karne ke liye use hota hai, aur iska scope block level hota hai
+// let and const are most commonly used keywords for declaring variables in modern JavaScript. The var keyword is still valid but is less commonly used due to its function-scoped behavior, which can lead to unexpected results in certain situations.
